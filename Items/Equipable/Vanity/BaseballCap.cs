1adfe51bf07295c4889fc6b61f744d3a0f99e5ba
goldenapple <goldenapplemc3@gmail.com>
2018-07-10 16:46:34 +0300

    Baseball Cap improvements

--- Items/Equipable/Vanity/BaseballCap.cs
+++ Items/Equipable/Vanity/BaseballCap.cs
@@ -16,11 +16,49 @@ namespace GoldensMisc.Items.Equipable.Vanity
 		
 		public override void SetDefaults()
 		{
-			item.width = 22;
+			item.width = 28;
 			item.height = 10;
 			item.rare = 1;
 			item.vanity = true;
 			item.value = Item.sellPrice(0, 0, 50);
+		}
+
+		public override bool CanRightClick()
+		{
+			return true;
+		}
+
+		public override void RightClick(Player player)
+		{
+			item.SetDefaults(mod.ItemType<BaseballCapBackwards>());
+		}
+	}
+
+	[AutoloadEquip(EquipType.Head)]
+	public class BaseballCapBackwards : ModItem
+	{
+		public override bool Autoload(ref string name)
+		{
+			return Config.BaseballBats;
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 28;
+			item.height = 10;
+			item.rare = 1;
+			item.vanity = true;
+			item.value = Item.sellPrice(0, 0, 50);
+		}
+
+		public override bool CanRightClick()
+		{
+			return true;
+		}
+
+		public override void RightClick(Player player)
+		{
+			item.SetDefaults();
 		}
 	}
 }
